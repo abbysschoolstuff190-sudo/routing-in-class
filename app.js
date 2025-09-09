c366a92ed95ce58fdfa5bbd885e32ce569ebec97
abbysschoolstuff190-sudo <abbysschoolstuff190@gmail.com>
2025-09-09 15:59:14 +0000

    added server framework with node.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const url = require('url');
 const availableTimes = {
     Monday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
     Tuesday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
@@ -9,3 +9,11 @@ const availableTimes = {
 const appointments = [
     {name: "James", day: "Wednesday", time: "3:30" },
     {name: "Lillie", day: "Friday", time: "1:00" }];
+
+const myserver = http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('<html> <body> <p> <strong> Hello World! </strong> </p> <p> Good Morning Class
+ </p> </body> </html>'); //write (send) a response to the client
+ res.end(); //end the response
+});
+myserver.listen(80); //the server object listens on port 80
